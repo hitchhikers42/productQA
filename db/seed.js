2b9be1dfb3033e1350b752cc732168afb25eb34e
Eivin Adlawan <eadlawa2@gmail.com>
2019-12-02 14:37:29 -0600

    Savepoint: creating my file directory

---
+++ db/seed.js
@@ -0,0 +1,10 @@
+//write things to seed the database. Use Faker.
+  //each question has a username
+    //each question has a question
+
+//Random Q&A on the page first.
+// One question: main answer > many answers
+
+//divided into different components if I want
+  //Post (outer container)
+    //Question and Answers
