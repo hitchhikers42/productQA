9c31d5027b2f9465b8960b00abb408f20ca221cc
Eivin Adlawan <eadlawa2@gmail.com>
2019-12-09 21:02:47 -0600

    Savepoint: able to pull database now from mongoDB

--- db/QASchema.js
+++ db/QASchema.js
@@ -4,10 +4,7 @@ const db = require('./index.js');
 const QASchema = new mongoose.Schema({
   productId: String,
   name: String,
-  posts: {
-    question: String,
-    answer: String
-  }
+  posts: []
 
 });
 
