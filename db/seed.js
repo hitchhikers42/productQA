21fcfc457291172f80dc6923510fb0dc6fc377b6
Eivin Adlawan <eadlawa2@gmail.com>
2019-12-13 18:20:45 -0600

    Savepoint: fixed 100

--- db/seed.js
+++ db/seed.js
@@ -1,21 +1,93 @@
-
 const QA = require('./QASchema.js');
 const faker = require('faker');
-console.log('SEED SCRIPT!!');
+
+/* returns a random number between 0 and given 'num' */
+const random = (num) =>
+  Math.floor((Math.random() * num) + 1);
 
 
+/* STEP 1 */
 /* Delete all entries if they exist */
+const deleteAll = (callback) => {
+  //delete all entries
+  QA.deleteMany({}, (err) => {
+      err ? callback(err) :
+        callback(null)
+  });
+};
 
-  const deleteAll = () => {
-    QA.deleteMany({}, (err) => {
-      err ? console.error(err) :
-        console.log('Successfully removed all records');
-    });
+/* STEP 2 */
+const generateProductIds = () => {
+  let productIds = [];
+
+  for (var value = 1; value <= 100; value++) {
+    productIds.push(`${value}`);
   }
-deleteAll()
+
+  return productIds;
+}
+
+/* STEP 3 */
+const generateData = (productIds) => {
+ /* Holds all of the records created */
+  sampleData = [];
+  productIds.forEach(id => {
+
+    let data = {
+      questions: [],
+      productId: id
+    }
+
+    let question = {
+      name: faker.internet.userName(),
+      question: faker.lorem.sentences(random(4)).slice(0, -1) + '?',
+      answers: []
+    }
+
+    for (let j = 0; j < random(4); j++) {
+      let answer = {
+        username: faker.internet.userName(),
+        answer: faker.lorem.sentences(random(9))
+      }
+      question.answers.push(answer)
+    }
+    data.questions.push(question)
+    sampleData.push(data);
+  })
+  return sampleData;
+}
+
+/* STEP 4 */
+const insertData = (sampleData) => {
+  QA.insertMany(sampleData, (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`successfully added ${sampleData.length} items.`)
+    }
+  })
+}
 
 
+/* STEP 5 -- invoke as callback */
 
+const seed = () => {
+  deleteAll(err => {
+    if(err) {
+      console.error(err);
+    } else {
+      let ids = generateProductIds();
+      let data = generateData(ids);
+      insertData(data);
+    }
+  })
+}
+seed();
+
+//! Push numbers into productsIds
+
+// const productIds = generateProductIds();
+// console.log(productIds)
 
 
   // const counter = 0;
@@ -30,62 +102,14 @@ deleteAll()
 
 
 
-  const productIds = [
-    'BES870XL',
-    'IVFWCT242DBWH',
-    'TOB-135N'
-  ]
+//   //?
+// const insertProducts = function() {
+//   data.create(productIds)
+//     .then(() => db.disconnect());
+// };
+// insertProducts();
 
-  //! Push numbers into productsIds
-  for (var value = 1; value <= 100; value++) {
-    productIds.push(`${value}`);
-  }
-
-  console.log(productIds)
-
-  /* returns a random number between 0 and given 'num' */
-  const random = (num) => Math.floor((Math.random() * num) + 1);
-
-  /* Holds all of the records created */
-  sampleData = [];
-
-
-
-  productIds.forEach(id => {
-    for (let i = 0; i < random(7); i++) {
-      let data = {
-        questions: [],
-        productId: id
-      }
-
-      let question = {
-        name: faker.internet.userName(),
-        question: faker.lorem.sentences(random(4)).slice(0, -1) + '?',
-        answers: []
-      }
-
-      for (let j = 0; j < random(4); j++) {
-        let answer = {
-          username: faker.internet.userName(),
-          answer: faker.lorem.sentences(random(9))
-        }
-        question.answers.push(answer)
-      }
-      data.questions.push(question)
-      sampleData.push(data);
-    }
-  })
-
-  QA.insertMany(sampleData, (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`successfully added ${sampleData.length} items.`)
-    }
-  })
-
-
-
+//? ^^^^^^
 
 
 /*
